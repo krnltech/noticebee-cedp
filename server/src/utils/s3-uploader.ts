684863aa05bc309941f1ce9acb1298de2b94381f
njayman <najishm828282@gmail.com>
2021-12-25 08:07:27 +0000

    removed env from uploader cred

--- server/src/utils/s3-uploader.ts
+++ server/src/utils/s3-uploader.ts
@@ -1,9 +1,9 @@
 import AWS from "aws-sdk";
-// const ID = "QI6NZMF168674QVS1GIT";
-// const SECRET = "XHv3hFZKfQANCTv83qSzsjDLP698hwR1VbVWIfFV";
+const ID = "QI6NZMF168674QVS1GIT";
+const SECRET = "XHv3hFZKfQANCTv83qSzsjDLP698hwR1VbVWIfFV";
 // const BUCKET_NAME = "kernel";
 
-const { ID, SECRET } = process.env;
+// const { ID, SECRET } = process.env;
 
 const s3 = new AWS.S3({
   accessKeyId: ID,
